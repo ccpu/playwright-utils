e31c97471ca4d4ddb8813ac3fe4d2a4045b71fcb
Cyrus-d <m.doaie@hotmail.com>
2020-04-20 18:09:41 +0430

    export mock

--- packages/browser/src/__tests__/mock/index.ts
+++ packages/browser/src/__tests__/mock/index.ts
@@ -1,1 +1,6 @@
-export * from '../../../../../__mocks__/playwright-core';
+// eslint-disable-next-line jest/no-mocks-import
+import {
+  browserMock,
+  pageMock,
+} from '../../../../../__mocks__/playwright-core';
+export { browserMock, pageMock };
