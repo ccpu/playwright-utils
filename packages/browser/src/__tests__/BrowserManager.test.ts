5efcf5b78fc4013a55213c2877afd91872a93ada
Cyrus-d <m.doaie@hotmail.com>
2020-04-28 13:53:22 +0430

    create browser if not exist when calling newPage

--- packages/browser/src/__tests__/BrowserManager.test.ts
+++ packages/browser/src/__tests__/BrowserManager.test.ts
@@ -11,7 +11,7 @@ describe('remote browser', () => {
   });
 
   afterEach(async () => {
-    await browser.close();
+    await browser.closeAll();
   });
 
   it('should throw error when browserTypes not provided', async () => {
@@ -57,12 +57,35 @@ describe('local Browser', () => {
   });
 
   afterEach(async () => {
-    await browser.close();
+    await browser.closeAll();
   });
 
   it('should have specified browsers', async () => {
     await browser.launch({ browserTypes: ['chromium', 'webkit', 'firefox'] });
 
     expect(browser.getBrowserInstances()).toHaveLength(3);
+  });
+
+  it('should return pages', async () => {
+    await browser.launch({ browserTypes: ['chromium', 'webkit', 'firefox'] });
+    const pages = await browser.newPages();
+    expect(pages).toHaveLength(3);
+  });
+
+  it('should create browser instance when calling newPage', async () => {
+    await browser.newPage('webkit');
+    expect(browser.getBrowser('webkit')).toBeDefined();
+  });
+
+  it('should close all browser', async () => {
+    await browser.launch({ browserTypes: ['chromium', 'webkit', 'firefox'] });
+    await browser.closeAll();
+    expect(browser.getBrowserInstances()).toHaveLength(0);
+  });
+
+  it('should close one browser', async () => {
+    await browser.launch({ browserTypes: ['chromium', 'webkit', 'firefox'] });
+    await browser.close('chromium');
+    expect(browser.getBrowserInstances()).toHaveLength(2);
   });
 });
